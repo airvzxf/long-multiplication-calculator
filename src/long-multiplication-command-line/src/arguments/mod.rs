9f87f52d99c74ccc0e28e2f28dcd2f19a3f2ab08
Israel Roldan <israel.alberto.rv@gmail.com>
2024-05-17 16:50:52 -0600

    Improved the arguments handler.

--- src/long-multiplication-command-line/src/arguments/mod.rs
+++ src/long-multiplication-command-line/src/arguments/mod.rs
@@ -1,20 +1,55 @@
-use clap::Parser;
+use clap::{Arg, ArgMatches, command};
 
-/// Create a table with the long-multiplication
-/// method given two values: the multiplicand
-/// and the multiplier.
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
 pub struct Args {
-    /// The first coefficient of the multiplication.
-    #[arg(long)]
+    // The first coefficient of the multiplication.
     pub multiplicand: String,
 
-    /// The second coefficient of the multiplication.
-    #[arg(long)]
+    // The second coefficient of the multiplication.
     pub multiplier: String,
+
+    // The output method.
+    pub output: String,
 }
 
 pub fn get_args() -> Args {
-    return Args::parse()
+    let matches: ArgMatches = command!()
+        .about("\
+            Create a table with the long-multiplication method given two values: \
+            the multiplicand and the multiplier."
+        )
+        .arg(
+            Arg::new("multiplicand")
+                .required(true)
+                .help("The first coefficient of the multiplication.")
+        )
+        .arg(
+            Arg::new("multiplier")
+                .required(true)
+                .help("The second coefficient of the multiplication.")
+        )
+        .arg(
+            Arg::new("output")
+                .short('o')
+                .long("output")
+                .required(false)
+                .default_value("display")
+                .help("The options are: 'display', 'store' or 'both'.")
+        )
+        .get_matches();
+
+    let multiplicand: String = unwrap_args(&matches, "multiplicand", false);
+    let multiplier: String = unwrap_args(&matches, "multiplier", false);
+    let output: String = unwrap_args(&matches, "output", true);
+
+    return Args { multiplicand, multiplier, output };
+}
+
+fn unwrap_args(matches: &ArgMatches, id: &str, lowercase: bool) -> String {
+    let value: String = matches.get_one::<String>(id).unwrap().to_string();
+
+    if lowercase {
+        return value.to_lowercase();
+    }
+
+    return value;
 }
